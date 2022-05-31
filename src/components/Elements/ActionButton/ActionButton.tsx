9ac58503119ad7474e9ddba6251c6c7c81e37336
naoki ota <on.s24jig@gmail.com>
2022-05-31 23:26:48 +0900

    ActionButtonをリファクタ

--- src/components/Elements/ActionButton/ActionButton.tsx
+++ src/components/Elements/ActionButton/ActionButton.tsx
@@ -1,6 +1,5 @@
 /** @jsxImportSource @emotion/react */
-import { Box, Center } from '@chakra-ui/react';
-import { css } from '@emotion/react';
+import { Box, Center, forwardRef } from '@chakra-ui/react';
 import clsx from 'clsx';
 import React from 'react';
 
@@ -9,34 +8,26 @@ export type ActionButtonProps =
     icon?: React.ReactElement;
   };
 
-export const ActionButton = React.forwardRef<
-  HTMLButtonElement,
-  ActionButtonProps
->(({ type = 'button', className = '', icon, ...props }, ref) => (
-  <button
-    ref={ref}
-    type={type}
-    className={clsx(className)}
-    css={css`
-      background: #ffa800;
-      border-radius: 9999px;
-      &:hover {
-        background: #f09e00;
-      }
-    `}
-  >
+export const ActionButton = forwardRef<ActionButtonProps, 'button'>(
+  ({ type = 'button', className = '', icon, ...props }, ref) => (
     <Center
-      paddingY={4}
-      pos="relative"
+      as="button"
+      ref={ref}
+      type={type}
+      className={clsx(className)}
+      py={4}
+      bg="accent.500"
       color="white"
       fontSize="md"
       fontWeight="bold"
-      _hover={{ color: 'whiteAlpha.900' }}
+      pos="relative"
+      rounded="full"
+      _hover={{ bg: 'accent.600', color: 'whiteAlpha.900' }}
     >
       <Center pos="absolute" left="14px">
         {icon}
       </Center>
       <Box>{props.children}</Box>
     </Center>
-  </button>
-));
+  )
+);
