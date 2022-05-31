ee43dbb064bd8ad03aa1ce737128adedf5a787c4
naoki ota <on.s24jig@gmail.com>
2022-05-31 23:25:50 +0900

    Chakra UIのテーマ調整

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -6,8 +6,38 @@ type AppProviderProps = {
   children: React.ReactNode;
 };
 
+const font =
+  'Helvetica, "Hiragino Kaku Gothic ProN", "Hiragino Sans", "Noto Sans JP", Arial, Meiryo, sans-serif';
+const theme = extendTheme({
+  fonts: {
+    heading: font,
+    body: font,
+  },
+  lineHeights: {
+    base: 1.4,
+  },
+  colors: {
+    base: {
+      500: '#ececec',
+    },
+    primary: {
+      300: '#2c689f',
+      500: '#1e5282',
+      700: '#0b3964',
+      900: '#061f35',
+    },
+    accent: {
+      500: '#ffa800',
+      600: '#f09e00',
+    },
+    favorite: {
+      500: '#ff5b5b',
+    },
+  },
+});
+
 export const AppProvider = ({ children }: AppProviderProps) => (
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <BrowserRouter>{children}</BrowserRouter>
   </ChakraProvider>
 );
