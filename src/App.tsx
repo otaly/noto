b91e9b2c8dde5fae9ecf973dec21d5b098b811b4
naoki ota <on.s24jig@gmail.com>
2022-06-02 00:21:49 +0900

    /, /mynotes, /favoritesへのルーティング追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,24 @@
 import React from 'react';
 import { RouteObject, useRoutes } from 'react-router-dom';
 import { MainLayout } from './components/Layout';
-import { NoteCardProps } from './features/notes/components/NoteCard';
-import { NoteCards } from './features/notes/components/NoteCards';
+import { Favorites } from './features/notes/routes/Favorites';
+import { Home } from './features/notes/routes/Home';
+import { MyNotes } from './features/notes/routes/MyNotes';
 import { AppProvider } from './providers/app';
-
-const Test = () => <p>This is Top</p>;
-const Test2 = () => <p>Here is Hoge</p>;
 
 const AppRoutes = () => {
   const routes: RouteObject[] = [
     {
       path: '/',
-      element: <Test />,
+      element: <Home />,
     },
     {
-      path: '/hoge',
-      element: <Test2 />,
+      path: '/mynotes',
+      element: <MyNotes />,
+    },
+    {
+      path: '/favorites',
+      element: <Favorites />,
     },
   ];
 
@@ -25,22 +27,10 @@ const AppRoutes = () => {
   return routeElement;
 };
 
-const notes: NoteCardProps[] = Array(8)
-  .fill(null)
-  .map((_, i) => ({
-    title: '今日の料理の計画について',
-    text: 'あ'.repeat(120 + i),
-    favoriteCount: 12 + i,
-    isFavorite: i % 2 === 0,
-  }));
-
 const App = () => (
   <AppProvider>
     <MainLayout>
       <AppRoutes />
-      <NoteCards notes={notes} />
-      {/* <Link to="hoge">Hoge</Link>
-      <Link to="/">Top</Link> */}
     </MainLayout>
   </AppProvider>
 );
