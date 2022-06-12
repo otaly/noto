6aab5836a09103d185da02d21e9f6c23ddb3a6dc
naoki ota <on.s24jig@gmail.com>
2022-06-12 23:11:36 +0900

    amplify add api

---
+++ src/graphql/queries.ts
@@ -0,0 +1,67 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getNote = /* GraphQL */ `
+  query GetNote($id: ID!) {
+    getNote(id: $id) {
+      id
+      title
+      content
+      authorId
+      type
+      updatedAt
+      createdAt
+    }
+  }
+`;
+export const listNotes = /* GraphQL */ `
+  query ListNotes(
+    $filter: ModelNoteFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listNotes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        title
+        content
+        authorId
+        type
+        updatedAt
+        createdAt
+      }
+      nextToken
+    }
+  }
+`;
+export const notesByDate = /* GraphQL */ `
+  query NotesByDate(
+    $type: String!
+    $updatedAt: ModelStringKeyConditionInput
+    $sortDirection: ModelSortDirection
+    $filter: ModelNoteFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    notesByDate(
+      type: $type
+      updatedAt: $updatedAt
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        title
+        content
+        authorId
+        type
+        updatedAt
+        createdAt
+      }
+      nextToken
+    }
+  }
+`;
